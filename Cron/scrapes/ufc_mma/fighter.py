ad531d4d380922a1a08e9daea203e8bf8f4f5101
ahmed-elafandi <aelafandi03@gmail.com>
2025-09-17 00:11:06 -0500

    My link functions (#12)

--- Cron/scrapes/ufc_mma/fighter.py
+++ Cron/scrapes/ufc_mma/fighter.py
@@ -32,15 +32,68 @@ def get_fighter_dob(soup: BeautifulSoup):
     dob_str = dob_label.find_next("span").get_text(strip=True)
     return datetime.strptime(dob_str, "%Y %b %d").date()
 
+def extract_career_stats(soup):
+    stats = {}
+    
+    # Find the "Career statistics" box
+    stat_items = soup.select("ul.b-list__box-list li.b-list__box-list-item")
+    
+    for item in stat_items:
+        label_tag = item.find("i", class_="b-list__box-item-title")
+        if label_tag:
+            label = label_tag.get_text(strip=True)
+            value = label_tag.next_sibling.strip() if label_tag.next_sibling else None
+            
+            if label and value:
+                stats[label] = value
+    
+    return stats
+
+
 ### Parsing logic END
+
+###
+
+def get_fighter_data_ufc_stats(event_url: str):
+    try: 
+        response = requests.get(event_url, headers=HEADERS)
+        print(response.status_code)
+        soup = BeautifulSoup(response.text, "html.parser")
+        data = extract_career_stats(soup = soup)
+        print(data)
+    except Exception as e: print(e)
+    # print(response.status_code)
+    # soup = BeautifulSoup(response.text, "html.parser")
+    # data = extract_career_stats(soup = soup)
+    # print(data)
+
+def get_fight_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    fight_links = []
+    for a in soup.find_all("a", class_="b-flag"):
+        href = a.get("href")
+        if href and "fight-details" in href:
+            fight_links.append(href)
+    
+    return fight_links
+
+
+###
 
 def get_fighter_data(event_url: str):
     response = requests.get(event_url, headers=HEADERS)
     soup = BeautifulSoup(response.text, "html.parser")
     link=get_ufcstats_link(soup_or_html=soup)
-    fighters_dob = get_fighter_dob(soup)
-    print(event_url)
-    print(link)
-    response = requests.get(link, headers=HEADERS)
-    print(response.status_code)
+    if link:
+        print(link)
+        get_fighter_data_ufc_stats(link)
+    else:
+        print('notwork')
+    #fighters_dob = get_fighter_dob(soup)
+    #print(event_url)
+    # print(link)
+    # response = requests.get(link, headers=HEADERS)
+    # print(response.status_code)
     # print(fighters_dob)
--- Cron/test.py
+++ Cron/test.py
@@ -1,4 +1,7 @@
 link = 'https://www.tapology.com/fightcenter/fighters/alex-perez'
 from scrapes.ufc_mma import get_all_events, get_event_data, get_fighter_data
 
-get_fighter_data(link)
+link2 = 'http://www.ufcstats.com/fighter-details/d549cefc7c54ab78'
+
+get_fighter_data(link2)
+
