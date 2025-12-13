60ba15d131a4b2409c2ff00c86f1a479e4c68606
Clay Ramey <car0126@auburn.edu>
2025-12-12 22:08:57 -0600

    t

--- Cron/ncaamb/run_ncaamb.sh
+++ Cron/ncaamb/run_ncaamb.sh
@@ -4,7 +4,7 @@ set -e
 # Absolute paths only (cron requires this)
 PROJECT_DIR="/home/caramey/prod"
 VENV_DIR="$PROJECT_DIR/venv"
-CRON_DIR="$PROJECT_DIR/Cron/ncaam"
+CRON_DIR="$PROJECT_DIR/Cron/ncaamb"
 
 # Activate virtual environment
 source "$VENV_DIR/bin/activate"
